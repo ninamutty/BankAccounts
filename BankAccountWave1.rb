450563dc27dfb993d48982edbca579dbe8b3a6a8
Nina Mutty <ninamutty@gmail.com>
2016-08-26 11:41:46 -0700

    Completed Basic requirements Wave 3

--- BankAccountWave1.rb
+++ BankAccountWave1.rb
@@ -57,27 +57,30 @@ module Bank
             @initial_balance = hash_of_one_account[:balance]
             @account_balance = @initial_balance
             @time_opened = hash_of_one_account[:date_time]
+            @min_balance = 0
+            @transaction_fee = 0
             #puts "You've initialized"
         end
 
         def withdraw(amount)
-            if (@account_balance - amount) < 0
-                puts "Warning: you cannot overdraw your account. Your balance is  still $#{"%.10g" % ("%.2f" % @account_balance)}."
+            if (@account_balance - amount) < @min_balance
+                puts "\nWarning: This withdrawal will result in your account having a balance less than $#{@min_balance - @transaction_fee}. We cannot allow you to make this tansaction. Your balance is still $#{'%.10g' % ('%.2f' % @account_balance)}."
             else
                 @account_balance -= amount
-                puts "Withdrew $#{amount} - Your new balance is: $#{("%.2f" % @account_balance)}"
+                @account_balance -= @transaction_fee
+                puts "Withdrew $#{amount} - Your new balance is: $#{('%.2f' % @account_balance)}"
             end
             return @account_balance
         end
 
         def deposit(amount)
             @account_balance += amount
-            puts "Deposited $#{amount} - Your new balance is: $#{("%.2f" % @account_balance)}"
+            puts "Deposited $#{amount} - Your new balance is: $#{('%.2f' % @account_balance)}"
             return @account_balance
         end
 
         def check_balance
-            puts "Your current balance is: $#{("%.2f" % @account_balance)}"
+            puts "Your current balance is: $#{('%.2f' % @account_balance)}"
         end
 
         ####### CLASS METHODS #########
@@ -126,14 +129,114 @@ module Bank
         end
     end
 
-    # class SavingsAccount < Account
-    #
-    # end
+    ######################################
+    ###  REQUIREMENTS: SavingsAccount  ###
+    ######################################
+    # Create a SavingsAccount class which should inherit behavior from the Account class. It should include the following updated functionality:
+
+    # The initial balance cannot be less than $10. If it is, this will raise an ArgumentError
+    # Updated withdrawal functionality:
+        # Each withdrawal 'transaction' incurs a fee of $2 that is taken out of the balance.
+        # Does not allow the account to go below the $10 minimum balance - Will output a warning message and return the original un-modified balance
+
+    # It should include the following new method:
+        # #add_interest(rate): Calculate the interest on the balance and add the interest to the balance. Return the interest that was calculated and added to the balance (not the updated balance).
+        # Input rate is assumed to be a percentage (i.e. 0.25).
+        # The formula for calculating interest is balance * rate/100
+            # Example: If the interest rate is 0.25% and the balance is $10,000, then the interest that is returned is $25 and the new balance becomes $10,025.
 
 
+    class SavingsAccount < Account
+
+        def initialize(hash_of_one_account) #(id, initial_balance, time_opened)
+            super
+            raise ArgumentError, ": cannot create an account with an initial balance less than $10.00" if hash_of_one_account[:balance] < 10
+        end
 
 
+        def withdraw(amount)
+            store_min_balance = @min_balance
+            @transaction_fee = 2
+            @min_balance += (10 + @transaction_fee)
+            puts "\nThere is a $#{@transaction_fee} charge for withdrawing money from a Savings Account."
+            super
+            @transaction_fee = 0
+            @min_balance = store_min_balance
+            return @account_balance
+        end
 
+
+        def add_interest(rate)
+            #balance * rate/100
+            @account_balance += @account_balance * rate/100
+            return "Your new balance is $#{('%.2f' % @account_balance)}"
+        end
+    end
+
+
+    #######################################
+    ###  REQUIREMENTS: CheckingAccount  ###
+    #######################################
+    # Create a CheckingAccount class which should inherit behavior from the Account class. It should include the following updated functionality:
+
+    # Updated withdrawal functionality:
+        # Each withdrawal 'transaction' incurs a fee of $1 that is taken out of the balance. Returns the updated account balance.
+            # Does not allow the account to go negative. Will output a warning message and return the original un-modified balance.
+        # #withdraw_using_check(amount): The input amount gets taken out of the account as a result of a checks withdrawal. Returns the updated account balance.
+            # Allows the account to go into overdraft up to -$10 but not any lower
+            # The user is allowed three free check uses in one month, but any subsequent use adds a $2 transaction fee
+        # #reset_checks: Resets the number of checks used to zero
+
+    class CheckingAccount < Account
+
+        def initialize(hash_of_one_account)
+            super
+            @checks = 0
+        end
+
+        def withdraw(amount)
+            store_min_balance = @min_balance
+            @transaction_fee = 1
+            @min_balance = (0 + @transaction_fee)
+            puts "\nThere is a $#{@transaction_fee} charge for withdrawing money from a Checking Account."
+            super
+            @transaction_fee = 0
+            @min_balance = store_min_balance
+            return @account_balance
+        end
+
+
+        def withdraw_using_check (amount)
+            store_min_balance = @min_balance
+            @checks += 1
+            if @checks <= 3
+                @transaction_fee = 1
+            else
+                puts "There is a $2.00 fee for using a check."
+                @transaction_fee = 3
+            end
+            @min_balance = (-10 + @transaction_fee)
+            if (@account_balance - amount) < @min_balance
+                puts "\nWarning: This withdrawal will result in your account having a balance less than $#{@min_balance - @transaction_fee}. We cannot allow you to make this tansaction. Your balance is still $#{'%.10g' % ('%.2f' % @account_balance)}."
+                @checks -= 1
+            else
+                @account_balance -= amount
+                @account_balance -= @transaction_fee
+                puts "Withdrew $#{amount} - Your new balance is: $#{('%.2f' % @account_balance)}"
+            end
+            puts "\nYou have used #{@checks} checks this month."
+            return @account_balance
+        end
+
+        def reset_checks
+            @checks = 0
+        end
+
+    end
+
+    ########################################
+    ###  OPTIONALS: MoneyMarketsAccount  ###
+    ########################################
 
 
 
@@ -162,10 +265,16 @@ module Bank
         end
 
 
+        #############################
+        ### EDIT BELOW 3 METHODS ####
+        #############################
+
         ####THIS ADDS TO A SPECIFIC OWNER - NOT JUST A NEW ACCOUNT
         def add_account
             @array_of_accounts.push(Account.new(account_hash))
         end
+
+
 
         def print_accounts
             count = 1
@@ -195,37 +304,38 @@ module Bank
                 @@owner_hash[owner_id] = self.new(new_owner)
             end
 
-            Bank::Account.gets_csv_info
-            # Bank::Account.pretty_print
-            @owners_by_account_id = {}
+            # Bank::Account.gets_csv_info
+            # # Bank::Account.pretty_print
+            # @owners_by_account_id = {}
 
-            CSV.open("support/account_owners.csv", "r").each do |line|
-                owner_id = line[1] #Owner id numbers
-                @account_id = line[0].to_i #Account id numbers
-                @owners_by_account_id[@account_id] = @@owner_hash[owner_id]
-                ####THIS IS A HASH WHERE THE KEY IS THE ACCOUNT ID NUMBER AND THE VALUE IS THE OWNER ID OWNER INFORMATION
+            # CSV.open("support/account_owners.csv", "r").each do |line|
+            #     owner_id = line[1] #Owner id numbers
+            #     @account_id = line[0].to_i #Account id numbers
+            #     @owners_by_account_id[@account_id] = @@owner_hash[owner_id]
+            #     ####THIS IS A HASH WHERE THE KEY IS THE ACCOUNT ID NUMBER AND THE VALUE IS THE OWNER ID OWNER INFORMATION
+            # end
+            #
+            # #Prints owner number 1213
+            # ap @owners_by_account_id[1212]
+            # ap
+            # #ap Bank::Account.find(1212)
+            # #ap @@account_hash  #### PRINT THE ACCOUNT HASH GENERATED IN THE ACCOUNT CLASS
+            #
+            #
+            #
+            # ##################
+            # ### STUCK HERE ###
+            # ##################
+            # @owners_by_account_id.each do |id|   ####### HOW DO I SHOVEL THIS INFORMATION IN??? ######
+            #     ### PSUEDOCODE
+            #     # in the owners_by_account_id hash - for each account_id number key, the value is a hash of the owner's information. I want to put their account information in that hash soooooo, we need to add an @account to each person's account with the correct account id - WITHOUT RESETING THE HASH VALUE
+            #
+            #     if id.@owner_id_number  #### WUUUUTTTT  ########
+            #         id.@accounts.push(@@account_hash[id])          #### UGH... ### THIS IS HARD...
+            #     end
+            # end
 
-            end
-
-            ap @owners_by_account_id[1213]
-
-            #ap Bank::Account.find(1212)
-            #ap @@account_hash  #### PRINT THE ACCOUNT HASH GENERATED IN THE ACCOUNT CLASS
-
-            ##################
-            ### STUCK HERE ###
-            ##################
-            @owners_by_account_id.each do |id|   ####### HOW DO I SHOVEL THIS INFORMATION IN??? ######
-                ### PSUEDOCODE
-                # in the owners_by_account_id hash - for each account_id number key, the value is a hash of the owner's information. I want to put their account information in that hash soooooo, we need to add @@account_hash with the correct account id - WITHOUT RESETING THE HASH VALUE
-                if id.owner_id_number == @@owner_hash.key    #### WUUUUTTTT  ########
-                    @accounts << Bank::Account.find(id)
-                end
-
-            end
         end
-
-
 
 
         def self.pretty_print ##Prints with strings - doesn't return a value - I wanted this because it was prettier..
@@ -266,7 +376,39 @@ end
 # puts account.time_opened
 
 
-Bank::Owner.gets_csv_info
+# Bank::Owner.gets_csv_info
+# puts "\nSAVINGS"
+# test_savings = Bank::SavingsAccount.new({account_id: 4444, balance: 10000, date_time: 02/04/2000})
+# ap test_savings.withdraw(600000)
+# ap test_savings.withdraw(500)
+# ap test_savings.add_interest(0.25)
+
+
+puts "\nCHECKING"
+test_savings = Bank::CheckingAccount.new({account_id: 4444, balance: 1000, date_time: 02/04/2000})
+#ap test_savings.withdraw(600000)
+#ap test_savings.withdraw(500)
+# puts "1"
+# ap test_savings.withdraw_using_check(1000)
+# ap test_savings.deposit(1000)
+
+# puts "2"
+# ap test_savings.withdraw_using_check(1010)
+# ap test_savings.deposit(1000)
+#
+# puts "3"
+# ap test_savings.withdraw_using_check(1009)
+# ap test_savings.deposit(1000)
+#
+# puts "4"
+# ap test_savings.withdraw_using_check(1)
+# ap test_savings.withdraw_using_check(1)
+# ap test_savings.withdraw_using_check(1)
+# ap test_savings.withdraw_using_check(1001)
+# #ap test_savings.deposit(1000)
+
+
+
 #ap Bank::Owner.all
 # puts "pretty print"
 # Bank::Owner.pretty_print
